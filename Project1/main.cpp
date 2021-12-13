d6d768f715fcd530d49dd8f5b155daf2bf3ac7d8
Александр <westerovs@mail.ru>
2021-12-13 23:59:47 +0300

    update 9

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,4 +5,12 @@ void main()
 {
 	setlocale(LC_ALL, "Rus");
 
+	int Var, Var2;
+
+	cout << "Please enter TWO numbers :" << endl;
+
+	cin >> Var >> Var2;
+
+	cout << "Numb1 === " << Var << endl << "Numb2 === " << Var2 << endl;
+
 }
